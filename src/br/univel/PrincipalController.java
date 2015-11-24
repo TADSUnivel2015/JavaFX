ddbc271e2de0fbe797191e13567dfabc01dfa74f
alex tezza <tezzaalex@gmail.com>
2015-11-24 20:27:22 -0200

    Novo arquivo.

---
+++ src/br/univel/PrincipalController.java
@@ -0,0 +1,45 @@
+package br.univel;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class PrincipalController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button salvar;
+
+    @FXML
+    private TextArea txfNome;
+
+    @FXML
+    private TextArea txfCodigo;
+
+    @FXML
+    void salvar(ActionEvent event) {
+    	String codigo = txfCodigo.getText();
+    	String nome = txfNome.getText();
+    	
+    	System.out.println(codigo + "\t" + nome);
+    }
+
+    @FXML
+    void initialize() {
+        assert salvar != null : "fx:id=\"salvar\" was not injected: check your FXML file 'Principal.fxml'.";
+        assert txfNome != null : "fx:id=\"txfNome\" was not injected: check your FXML file 'Principal.fxml'.";
+        assert txfCodigo != null : "fx:id=\"txfCodigo\" was not injected: check your FXML file 'Principal.fxml'.";
+
+    }
+}
+
+
